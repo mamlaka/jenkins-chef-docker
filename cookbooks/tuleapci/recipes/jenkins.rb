5f3c27cea6432fda53a5296b70a6125e8a2fe08f
Manuel Vacelet <manuel.vacelet@enalean.com>
2014-10-02 17:41:04 +0200

    No need to set group (will crash if jclouds configured to grant sudo

--- cookbooks/tuleapci/recipes/jenkins.rb
+++ cookbooks/tuleapci/recipes/jenkins.rb
@@ -1,6 +1,5 @@
 file "/jenkins/.ssh/known_hosts" do
     owner "jenkins"
-    group "jenkins"
     content node['users']['jenkins']['known_hosts'].join("\n")
     mode 00600
     action :create_if_missing
@@ -9,7 +8,6 @@ end
 # Add ssh private key
 file "/jenkins/.ssh/id_rsa" do
   owner   "jenkins"
-  group   "jenkins"
   mode    00600
   content node['users']['jenkins']['rsa']
   action  :create_if_missing
@@ -17,7 +15,6 @@ end
 
 file "/jenkins/.ssh/id_rsa.pub" do
   owner   "jenkins"
-  group   "jenkins"
   mode    00600
   content node['users']['jenkins']['rsa.pub']
   action  :create_if_missing
